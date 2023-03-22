4c65b4101e89bb45690cc885b88f0d7b6640b3e7
IzumiSy <beetle-noise@gmx.com>
2023-03-22 21:25:56 +0900

    Prettified

--- src/firestore.ts
+++ src/firestore.ts
@@ -1,24 +1,24 @@
-import {Session} from '@shopify/shopify-api'
-import {SessionStorage} from '@shopify/shopify-app-session-storage'
+import { Session } from "@shopify/shopify-api";
+import { SessionStorage } from "@shopify/shopify-app-session-storage";
 
 export class FirestoreSessionStorage implements SessionStorage {
-    storeSession(session: Session): Promise<boolean> {
-        throw new Error('Method not implemented.')
-    }
+  storeSession(session: Session): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
 
-    loadSession(id: string): Promise<Session | undefined> {
-        throw new Error('Method not implemented.')
-    }
+  loadSession(id: string): Promise<Session | undefined> {
+    throw new Error("Method not implemented.");
+  }
 
-    deleteSession(id: string): Promise<boolean> {
-        throw new Error('Method not implemented.')
-    }
+  deleteSession(id: string): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
 
-    deleteSessions(ids: string[]): Promise<boolean> {
-        throw new Error('Method not implemented.')
-    }
+  deleteSessions(ids: string[]): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
 
-    findSessionsByShop(shop: string): Promise<Session[]> {
-        throw new Error('Method not implemented.')
-    }
+  findSessionsByShop(shop: string): Promise<Session[]> {
+    throw new Error("Method not implemented.");
+  }
 }
