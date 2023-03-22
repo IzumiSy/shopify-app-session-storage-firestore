d71cbd5db5f52c71133e7a0214a63950c81e9e29
IzumiSy <beetle-noise@gmx.com>
2023-03-22 23:32:16 +0900

    Implemented methods: storeSession and loadSession

--- src/firestore.ts
+++ src/firestore.ts
@@ -1,13 +1,68 @@
-import { Session } from "@shopify/shopify-api";
+import { Session, SessionParams } from "@shopify/shopify-api";
 import { SessionStorage } from "@shopify/shopify-app-session-storage";
+import { FirebaseOptions, initializeApp } from "firebase/app";
+import {
+  DocumentData,
+  FieldValue,
+  Firestore,
+  FirestoreDataConverter,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+  WithFieldValue,
+  doc,
+  getDoc,
+  getFirestore,
+  setDoc,
+} from "firebase/firestore";
+
+type SerializedSession = ReturnType<
+  InstanceType<typeof Session>["toPropertyArray"]
+>;
+type SessionDocument = {
+  shopID: string;
+  value: SerializedSession;
+};
 
 export class FirestoreSessionStorage implements SessionStorage {
-  storeSession(session: Session): Promise<boolean> {
-    throw new Error("Method not implemented.");
+  private firestore: Firestore;
+  private converter: FirestoreDataConverter<SessionDocument>;
+
+  constructor(firebaseConfiguration: FirebaseOptions) {
+    const app = initializeApp(firebaseConfiguration);
+    this.firestore = getFirestore(app);
+    this.converter = {
+      toFirestore(data: WithFieldValue<SessionDocument>): DocumentData {
+        return data;
+      },
+
+      fromFirestore(
+        snapshot: QueryDocumentSnapshot<SessionDocument>,
+        options?: SnapshotOptions
+      ): SessionDocument {
+        return snapshot.data(options);
+      },
+    };
   }
 
-  loadSession(id: string): Promise<Session | undefined> {
-    throw new Error("Method not implemented.");
+  async storeSession(session: Session): Promise<boolean> {
+    await setDoc(
+      doc(this.firestore, "__sessions").withConverter(this.converter),
+      {
+        shopID: session.shop,
+        value: session.toPropertyArray(),
+      }
+    );
+    return true;
+  }
+
+  async loadSession(id: string): Promise<Session | undefined> {
+    const snapshot = await getDoc<SessionDocument>(
+      doc(this.firestore, "__sessions", id).withConverter(this.converter)
+    );
+    if (snapshot.exists()) {
+      const data = snapshot.data();
+      return Session.fromPropertyArray(data.value);
+    }
   }
 
   deleteSession(id: string): Promise<boolean> {
